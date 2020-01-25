f595472da90d1465f21a837b88bf5416f63b7c3c
dinodev76 <dinodev76@outlook.com>
2020-01-25 14:00:33 -0800

    First release - v0.1

--- READREL.cob
+++ READREL.cob
@@ -1,9 +1,9 @@
-      *=========================== READIDX ============================*
+      *=========================== READREL ============================*
       * Authors: Brian D Pead
       *
       * License: MIT
       *
-      * Description: Test reading the indexed file created by program
+      * Description: Test reading the relative file created by program
       *              BLDFILES.
       *
       * Date        Version  Description
@@ -14,7 +14,7 @@
        IDENTIFICATION DIVISION.
       *========================
 
-       PROGRAM-ID.                 READIDX.
+       PROGRAM-ID.                 READREL.
 
        ENVIRONMENT DIVISION.
       *=====================
@@ -30,12 +30,10 @@
       *---------------------
 
        FILE-CONTROL.
-           SELECT NAMADDIX         ASSIGN "Data\NAMEADDR.Idx.dat"
-                                   ORGANIZATION INDEXED   
+           SELECT NAMADDRL         ASSIGN "Data\NAMEADDR.Rel.dat"
+                                   ORGANIZATION RELATIVE   
                                    ACCESS DYNAMIC
-                                   RECORD KEY NA-TAXID
-                                   ALTERNATE KEY NA-LAST-NAME
-                                       WITH DUPLICATES
+                                   RELATIVE KEY W-RELATIVE-REC
                                    FILE STATUS W-FILE-STATUS.
       /
        DATA DIVISION.
@@ -44,36 +42,29 @@
        FILE SECTION.
       *-------------
 
-       FD  NAMADDIX.
+       FD  NAMADDRL.
 
-       01  NAMADDIX-REC.           COPY NAMEADDR.
+       01  NAMADDRL-REC.           COPY NAMEADDR.
 
        WORKING-STORAGE SECTION.
       *------------------------
 
-       01  W-NAMADDIX-RECS         PIC 9(09)  COMP VALUE 0.
+       01  W-NAMADDRL-RECS         PIC 9(09)  COMP VALUE 0.
        01  W-RETURN-CODE           PIC S9(04) COMP.
        01  W-DISP-NUM              PIC ZZ,ZZ9.
 
        01  W-ERROR-MSG             PIC X(20)       VALUE
-           '**** READIDX error: '.
+           '**** READREL error: '.
 
        01  W-READ-METHOD           PIC X(01).
-           88  W-READ-METHOD-RANDOM                VALUE 'R'.
+           88  W-READ-METHOD-RELATIVE              VALUE 'R'.
            88  W-READ-METHOD-SEQUENTIAL            VALUE 'S'.
            88  W-READ-METHOD-EXIT                  VALUE ' '.
            88  W-READ-METHOD-VALID                 VALUE 'R'
                                                          'S'
                                                          ' '.
-       01  W-READ-KEY-FIELD              PIC X(01).
-           88  W-READ-KEY-FIELD-LAST-NAME          VALUE 'L'.
-           88  W-READ-KEY-FIELD-TAXID              VALUE 'T'.
-           88  W-READ-KEY-FIELD-EXIT               VALUE ' '.
-           88  W-READ-KEY-FIELD-VALID              VALUE 'L'
-                                                         'T' 
-                                                         ' '.
-       01  W-KEY-VALUE             PIC X(35).
-           88  W-KEY-VALUE-EXIT                    VALUE SPACES.
+       01  W-RELATIVE-REC          PIC 9(09).
+           88  W-RELATIVE-REC-EXIT                 VALUE 0.
 
        01  FILLER                  PIC X(01).
            88  W-INVALID-KEY                       VALUE 'Y'.
@@ -126,7 +117,7 @@
            MOVE FUNCTION WHEN-COMPILED 
                                    TO W-COMPILED-DATE
 
-           DISPLAY 'READIDX compiled on '
+           DISPLAY 'READREL compiled on '
                W-COMPILED-DATE-YYYY '/'
                W-COMPILED-DATE-MM   '/'
                W-COMPILED-DATE-DD   ' at '
@@ -134,13 +125,13 @@
                W-COMPILED-TIME-MM   ':'
                W-COMPILED-TIME-SS
 
-           OPEN INPUT NAMADDIX
+           OPEN INPUT NAMADDRL
 
            IF      NOT W-FILE-STATUS-GOOD
                DISPLAY W-ERROR-MSG
                        'File status '
                        W-FILE-STATUS
-                       ' opening NAMADDIX'
+                       ' opening NAMADDRL'
                MOVE 10             TO W-RETURN-CODE
            END-IF
            .
@@ -150,16 +141,16 @@
        SUB-2000-PROCESS-READ-METHOD.
       *-----------------------------
 
-           PERFORM SUB-9200-GET-READ-KEY-FIELD THRU SUB-9200-EXIT
+           IF      W-READ-METHOD-RELATIVE
+               PERFORM SUB-9200-GET-RELATIVE-REC THRU SUB-9200-EXIT
 
-           IF      W-READ-METHOD-RANDOM
-               PERFORM SUB-9300-GET-KEY-VALUE THRU SUB-9300-EXIT
-
-               PERFORM SUB-2100-READ-RANDOM THRU SUB-2100-EXIT
-                   UNTIL W-KEY-VALUE-EXIT
+               PERFORM SUB-2100-READ-RELATIVE THRU SUB-2100-EXIT
+                   UNTIL W-RELATIVE-REC-EXIT
            ELSE
+               SET  W-NOT-EOF      TO TRUE
+
                PERFORM SUB-2200-READ-SEQUENTIAL THRU SUB-2200-EXIT
-                   UNTIL W-READ-KEY-FIELD-EXIT
+                   UNTIL W-EOF
                    OR    W-RETURN-CODE NOT = 0
            END-IF
 
@@ -172,16 +163,16 @@
        SUB-2000-EXIT.
            EXIT.
       /
-       SUB-2100-READ-RANDOM.
-      *---------------------
+       SUB-2100-READ-RELATIVE.
+      *-----------------------
 
-           PERFORM SUB-9400-READ-NAMADDIX-RANDOM THRU SUB-9400-EXIT 
+           PERFORM SUB-9300-READ-NAMADDRL-RELATIVE THRU SUB-9300-EXIT 
 
            IF      W-NOT-INVALID-KEY
-               DISPLAY NAMADDIX-REC
+               DISPLAY NAMADDRL-REC
            END-IF
 
-           PERFORM SUB-9300-GET-KEY-VALUE THRU SUB-9300-EXIT
+           PERFORM SUB-9200-GET-RELATIVE-REC THRU SUB-9200-EXIT
            .
        SUB-2100-EXIT.
            EXIT.
@@ -189,28 +180,20 @@
        SUB-2200-READ-SEQUENTIAL.
       *-------------------------
 
-           PERFORM SUB-9500-START-NAMADDIX THRU SUB-9500-EXIT
+           PERFORM SUB-9400-START-NAMADDRL THRU SUB-9400-EXIT
 
            IF      W-RETURN-CODE NOT = 0
                GO TO SUB-2200-EXIT
            END-IF
 
-           SET  W-NOT-EOF          TO TRUE
-
-           PERFORM SUB-9600-READ-NAMADDIX-SEQ THRU SUB-9600-EXIT
+           PERFORM SUB-9500-READ-NAMADDRL-SEQ THRU SUB-9500-EXIT
 
            PERFORM UNTIL W-EOF
                    OR    W-RETURN-CODE NOT = 0
-               DISPLAY NAMADDIX-REC
+               DISPLAY NAMADDRL-REC
 
-               PERFORM SUB-9600-READ-NAMADDIX-SEQ THRU SUB-9600-EXIT
+               PERFORM SUB-9500-READ-NAMADDRL-SEQ THRU SUB-9500-EXIT
            END-PERFORM
-
-           IF      W-RETURN-CODE NOT = 0
-               GO TO SUB-2200-EXIT
-           END-IF
-
-           PERFORM SUB-9200-GET-READ-KEY-FIELD THRU SUB-9200-EXIT
            .
        SUB-2200-EXIT.
            EXIT.
@@ -218,13 +201,13 @@
        SUB-3000-SHUT-DOWN.
       *-------------------
       
-           CLOSE NAMADDIX
+           CLOSE NAMADDRL
 
-           MOVE W-NAMADDIX-RECS    TO W-DISP-NUM
-           DISPLAY 'NAMADDIX records read:    '
+           MOVE W-NAMADDRL-RECS    TO W-DISP-NUM
+           DISPLAY 'NAMADDRL records read:    '
                    W-DISP-NUM
 
-           DISPLAY 'READIDX Completed'
+           DISPLAY 'READREL Completed'
            .
        SUB-3000-EXIT.
            EXIT.
@@ -235,7 +218,7 @@
            MOVE 'X'                TO W-READ-METHOD
 
            PERFORM UNTIL W-READ-METHOD-VALID        
-               DISPLAY 'Enter R for random reads, S for sequential, '
+               DISPLAY 'Enter R for relative reads, S for sequential, '
                        'space to exit: '
                    NO ADVANCING
            
@@ -245,123 +228,82 @@
        SUB-9100-EXIT.
            EXIT.
       /
-       SUB-9200-GET-READ-KEY-FIELD.
-      *----------------------------
-       
-           MOVE 'X'                TO W-READ-KEY-FIELD
-
-           PERFORM UNTIL W-READ-KEY-FIELD-VALID        
-               DISPLAY 'Enter L for last name key, T for taxid, '
-                       'space to exit: '
-                   NO ADVANCING
+       SUB-9200-GET-RELATIVE-REC.
+      *--------------------------
            
-               ACCEPT W-READ-KEY-FIELD
-           END-PERFORM
+           DISPLAY 'Enter relative record number (odd number since we '
+                   'left space in file for the even ones) or space to '
+                   ' exit: '
+               NO ADVANCING
+           
+           ACCEPT W-RELATIVE-REC
            .
        SUB-9200-EXIT.
            EXIT.
       /
-       SUB-9300-GET-KEY-VALUE.
-      *-----------------------
-           
-           DISPLAY 'Enter key value or space to exit: '
-               NO ADVANCING
-           
-           ACCEPT W-KEY-VALUE
-           .
-       SUB-9300-EXIT.
-           EXIT.
-      /
-       SUB-9400-READ-NAMADDIX-RANDOM.
-      *------------------------------
+       SUB-9300-READ-NAMADDRL-RELATIVE.
+      *--------------------------------
       
-           IF      W-READ-KEY-FIELD-LAST-NAME
-               MOVE W-KEY-VALUE    TO NA-LAST-NAME
-
-               READ NAMADDIX
-                   KEY NA-LAST-NAME
-                   INVALID KEY
-                       SET  W-INVALID-KEY
+           READ NAMADDRL
+               INVALID KEY
+                   SET  W-INVALID-KEY
                                    TO TRUE
-                       DISPLAY 'Key not on file: '
-                               NA-LAST-NAME
-                       GO TO SUB-9400-EXIT
-               END-READ
-           ELSE
-               MOVE W-KEY-VALUE    TO NA-TAXID
-               
-               READ NAMADDIX
-                   KEY NA-TAXID
-                   INVALID KEY
-                       SET  W-INVALID-KEY
-                                   TO TRUE
-                       DISPLAY 'Key not on file: '
-                               NA-TAXID
-                       GO TO SUB-9400-EXIT
-               END-READ
-           END-IF
+                   DISPLAY 'Relative record not on file: '
+                           W-RELATIVE-REC
+                   GO TO SUB-9300-EXIT
+           END-READ
 
            IF      W-FILE-STATUS-GOOD
                SET  W-NOT-INVALID-KEY
                                    TO TRUE
-               ADD  1              TO W-NAMADDIX-RECS
+               ADD  1              TO W-NAMADDRL-RECS
            ELSE    
                DISPLAY W-ERROR-MSG
                        'File status '
                        W-FILE-STATUS
-                       ' reading NAMADDIX key'
+                       ' reading NAMADDRL key'
                MOVE 20             TO W-RETURN-CODE
            END-IF
            .
-       SUB-9400-EXIT.
+       SUB-9300-EXIT.
            EXIT.
       /
-       SUB-9500-START-NAMADDIX.
+       SUB-9400-START-NAMADDRL.
       *------------------------
 
-           IF      W-READ-KEY-FIELD-LAST-NAME
-               MOVE LOW-VALUES     TO NA-LAST-NAME
-
-               START NAMADDIX
-                   KEY >= NA-LAST-NAME
-               END-START
-           ELSE
-               MOVE LOW-VALUES     TO NA-TAXID
-
-               START NAMADDIX
-                   KEY >= NA-TAXID
-               END-START
-           END-IF
+           START NAMADDRL
+               FIRST
+           END-START
 
            IF      NOT W-FILE-STATUS-GOOD
                DISPLAY W-ERROR-MSG
                        'File status '
                        W-FILE-STATUS
-                       ' starting NAMADDIX'
+                       ' starting NAMADDRL'
                MOVE 40             TO W-RETURN-CODE
            END-IF
            .
-       SUB-9500-EXIT.
+       SUB-9400-EXIT.
            EXIT.
       /
-       SUB-9600-READ-NAMADDIX-SEQ.
+       SUB-9500-READ-NAMADDRL-SEQ.
       *---------------------------
       
-           READ NAMADDIX NEXT
+           READ NAMADDRL NEXT
                AT END
                    SET  W-EOF      TO TRUE
-                   GO TO SUB-9600-EXIT
+                   GO TO SUB-9500-EXIT
            END-READ
 
            IF      W-FILE-STATUS-GOOD
-               ADD  1              TO W-NAMADDIX-RECS
+               ADD  1              TO W-NAMADDRL-RECS
            ELSE    
                DISPLAY W-ERROR-MSG
                        'File status '
                        W-FILE-STATUS
-                       ' reading NAMADDIX next record'
+                       ' reading NAMADDRL next record'
                MOVE 50             TO W-RETURN-CODE
            END-IF
            .
-       SUB-9600-EXIT.
+       SUB-9500-EXIT.
            EXIT.
